96db19611584e17f0d8c3caaa621b8b8650b6a37
pankaj-mvit <pankaj.mvit@hotmail.com>
2022-07-03 20:19:00 +0530

    3. Services Section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ServicesSection from "./components/SevicesSection";
 import AboutUs from "./Pages/AboutUs";
 function App() {
   return (
     <div className="App">
      <AboutUs/>
+     <ServicesSection/>
     </div>
   );
 }
